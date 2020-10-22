dbb820678476dafc0ec838c6a2900ea1922ba74d
m-sureshraj <m.s.suresh100@gmail.com>
2020-10-22 13:55:57 +0530

    refactor: Convert *.js files inside the lib/cli to .ts

---
+++ lib/cli/Types.ts
@@ -0,0 +1,34 @@
+export interface Key {
+  name: string;
+  ctrl: boolean;
+  meta: boolean;
+}
+
+// Fixme: mocha/load-options.js should provide the following type
+export interface MochaOptions {
+  [key: string]: string;
+}
+
+// Fixme: mocha/collect-files.js should provide the following type
+export interface Files {
+  collectedSpecFiles: string[];
+  filesGivenThroughFileOption: string[];
+}
+
+// Fixme: mocha/run.js should provide the following types
+export enum RunnerState {
+  running = 'running',
+}
+
+type Runner = { abort: () => Runner; state: RunnerState };
+
+export interface ReRunner {
+  run: (files: string[]) => void;
+  scheduleRun: (files: string[]) => void;
+  getRunner: () => null | Runner;
+}
+
+export interface Run {
+  watcher: NodeJS.EventEmitter;
+  reRunner: ReRunner;
+}
